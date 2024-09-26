74632ef927ca41d22309ead2a3213277bdd0ab30
Philemon Komi <philemonkomi46@gmail.com>
2024-09-26 13:00:20 +0300

    Added: Express Server, MongoDB+Connection, Set APIs Endpoints

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import Product from "./models/product.model.js";
+
+const app = express();
+
+//MIDDLEWARES
+app.use(express.json())
+
+
+//MONGOOSE CONNECTION
+const port = 9073;
+mongoose.connect("mongodb+srv://philemonkomi46:j2yx3KMIiaXnG7qV@backenddb.uaor4.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Conneced to Database")
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+})
+.catch(() => {
+    console.log("Conneced Failed!")
+
+})
+
+
+//API's
+app.get('/', (req, res) => {
+    res.send("HELLO FROM BACKEND SERVER");
+});
+
+app.post('/api/products', async (req, res) => {   //whenever we use await, we use async function
+    try {
+        const product = await Product.create(req.body); //takes time to analyze
+        res.status(200).send(product)
+        console.log("Product added successfully to database!")
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+    //API to view all my created 
---
+++ models/product.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter prduct name"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      deault: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true, //allows us to have two extra fields: when the field is created at and updated at
+  }
+);
+const Product = mongoose.model("Product", ProductSchema);
+
+//export the model
+export default Product;
+ 
